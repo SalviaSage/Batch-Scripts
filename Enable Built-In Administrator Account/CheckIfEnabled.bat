507f12da70d01b4bc74df9a1ede13f09b20b7ea9
Salvia <SalviaSage@users.noreply.github.com>
2020-04-02 14:12:04 -0400

    Create CheckIfEnabled.bat

---
+++ Enable Built-In Administrator Account/CheckIfEnabled.bat
@@ -0,0 +1,5 @@
+:: There is a built in administrator account in Windows 10. It is disabled by default.
+:: This checks if the built-in administrator account is enabled.
+
+net user administrator
+pause
