47eb7874ee742d8c93543156c1ceb7631df3b9ae
Salvia <SalviaSage@users.noreply.github.com>
2021-04-18 01:35:35 -0400

    Add files via upload

---
+++ ConnectWiFiConnection.bat
@@ -0,0 +1,5 @@
+:: This will connect to a set network name. The name of the network must be set on the below line.
+
+@echo off
+
+netsh wlan connect name="WifiNetWorkName"
---
+++ DisconnectWiFiConnection.bat
@@ -0,0 +1,5 @@
+:: This will disconnect the computer from the wireless network that it is connected to.
+
+@echo off
+
+netsh wlan disconnect
