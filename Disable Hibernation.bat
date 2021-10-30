3f1f54195ecab736a5f4706781d56b8e96402a59
Salvia <SalviaSage@users.noreply.github.com>
2021-10-30 14:30:39 -0400

    Delete Disable Hibernation.bat

--- Disable Hibernation.bat
+++
@@ -1,6 +0,0 @@
-:: This will disable the hibernation option on the power menu at the start menu.
-:: Must be ran as administrator, (in an elevated command prompt).
-
-@echo off
-
-powercfg /hibernate off
