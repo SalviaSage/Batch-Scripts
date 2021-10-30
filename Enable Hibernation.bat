29eb4bc327be491ee08f9bc39f38953a906d5f78
Salvia <SalviaSage@users.noreply.github.com>
2021-10-30 14:30:46 -0400

    Delete Enable Hibernation.bat

--- Enable Hibernation.bat
+++
@@ -1,6 +0,0 @@
-:: This will enable the hibernation option on the power menu at the start menu.
-:: Must be ran as administrator, (in an elevated command prompt).
-
-@echo off
-
-powercfg /hibernate on
