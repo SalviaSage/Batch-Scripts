a4107d97ad8d0ddb841ee736344d3e93de7f3e2d
Salvia <SalviaSage@users.noreply.github.com>
2023-09-27 14:34:58 -0400

    Add files via upload

---
+++ Hibernate.bat
@@ -0,0 +1,6 @@
+:: Hibernates the system.
+:: Press a key on the keyboard or click the mouse to awaken the system.
+
+@echo off
+
+shutdown -h
---
+++ User Complete Sign Out.bat
@@ -0,0 +1,5 @@
+:: User complete sign out. It is different than simply logging off.
+
+@echo off
+
+shutdown -l
